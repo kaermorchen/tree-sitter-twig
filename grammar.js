36bb973d38271e8ca87d923c43b9ad77e22cfcc6
Stanislav Romanov <kaermorchen@gmail.com>
2023-06-23 13:29:28 +0300

    Add basic directives

--- grammar.js
+++ grammar.js
@@ -1,7 +1,32 @@
 module.exports = grammar({
-  name: "twig",
-
+  name: 'twig',
+  extras: () => [/\s+/],
   rules: {
-    source_file: ($) => "",
+    template: ($) =>
+      repeat(
+        choice($.statement_directive, $.output_directive, $.comment, $.content)
+      ),
+
+    content: () => prec.right(repeat1(/[^\{]+|\{/)),
+
+    statement_directive: ($) =>
+      seq(
+        choice('{%', '{%-', '{%~'),
+        optional($._statement),
+        choice('%}', '-%}', '~%}')
+      ),
+
+    output_directive: ($) =>
+      seq(
+        choice('{{', '{{-', '{{~'),
+        optional($._statement),
+        choice('}}', '-}}', '~}}')
+      ),
+
+    comment: () => seq('{#', /[^#]*\#+([^\}#][^#]*\#+)*/, '}'),
+
+    _statement: ($) => choice($.assignment_statement),
+
+    assignment_statement: () => seq('set'),
   },
 });
