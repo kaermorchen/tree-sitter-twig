60ea58ad3b4b5a6cf14bb735d8bb300806304bf4
Stanislav Romanov <kaermorchen@gmail.com>
2023-07-27 16:23:01 +0300

    Fix bugs

--- src/scanner.c
+++ src/scanner.c
@@ -15,72 +15,57 @@ void tree_sitter_twig_external_scanner_deserialize(void *p, const char *b, unsig
 
 static void advance(TSLexer *lexer) { lexer->advance(lexer, false); }
 
-static bool scan_twig_content(TSLexer *lexer) {
-  lexer->result_symbol = CONTENT;
-
-  bool has_content = false;
-
-  while (lexer->lookahead) {
-    switch (lexer->lookahead) {
-      case '{':
-        advance(lexer);
-        if(lexer->lookahead == '{' ||
-           lexer->lookahead == '%' ||
-           lexer->lookahead == '#') {
-          return has_content;
-        }
-        break;
-      default:
-        advance(lexer);
-    }
-
-    lexer->mark_end(lexer);
-    has_content = true;
-  }
-
-  return has_content;
-}
-
-static bool scan_twig_comment(TSLexer *lexer) {
-  lexer->result_symbol = COMMENT;
-
-  if (lexer->lookahead != '{') return false;
-  lexer->mark_end(lexer);
-  advance(lexer);
-
-  if (lexer->lookahead != '#') return false;
-  advance(lexer);
-
-  // Consume characters until we read `#}`
-  while (lexer->lookahead) {
-    if(lexer->lookahead == '#') {
-      advance(lexer);
-
-      if(lexer->lookahead == '}') {
-        advance(lexer);
-        lexer->mark_end(lexer);
-        return true;
-      }
-    }
-
-    advance(lexer);
-  }
-
-  return true;
-}
-
 bool tree_sitter_twig_external_scanner_scan(void *payload, TSLexer *lexer, const bool *valid_symbols) {
   // Eat whitespace
   while (iswspace(lexer->lookahead)) {
     lexer->advance(lexer, true);
   }
 
-  if (valid_symbols[COMMENT] && lexer->lookahead == '{') {
-    return scan_twig_comment(lexer);
+  // CONTENT
+  bool has_content = false;
+
+  while (lexer->lookahead) {
+    if(lexer->lookahead == '{') {
+      advance(lexer);
+
+      if(lexer->lookahead == '{' ||
+        lexer->lookahead == '%' ||
+        lexer->lookahead == '#') {
+        break;
+      }
+    } else {
+      advance(lexer);
+    }
+
+    lexer->mark_end(lexer);
+    has_content = true;
   }
 
-  if (valid_symbols[CONTENT]) {
-    return scan_twig_content(lexer);
+  if (has_content) {
+    lexer->result_symbol = CONTENT;
+    return true;
+  }
+
+  // COMMENT
+  if (lexer->lookahead == '#') {
+    advance(lexer);
+
+    while (lexer->lookahead) {
+      lexer->mark_end(lexer);
+
+      if(lexer->lookahead == '#') {
+        advance(lexer);
+
+        if(lexer->lookahead == '}') {
+          lexer->result_symbol = COMMENT;
+          advance(lexer);
+          lexer->mark_end(lexer);
+          return true;
+        }
+      }
+
+      advance(lexer);
+    }
   }
 
   return false;
